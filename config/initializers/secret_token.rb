0b26956f7b8aee702dc96a7c89d3ad46aa589453
johnfarmernyc <johnfarmernyc@gmail.com>
2013-05-26 23:30:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easycare::Application.config.secret_token = 'c74bbcd8a834c1c7a7913736487d9caf548ab2dec7945f246406c3cfcea9e2329a9dbda212478582ec3af7a05326ee4aee78c02bbcb2298cce639875ac6fa068'
